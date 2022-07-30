9411b8f403e6aee23b2e0d521d05e43fae0e5714
Claudiu <clau_123@yahoo.com>
2022-07-30 16:28:48 +0300

    map with dummy data

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ProfileScreen from './screens/ProfileScreen';
+import MapScreen from './screens/MapScreen';
+import { NavigationContainer } from '@react-navigation/native';
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Map" component={MapScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/MapScreen.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StyleSheet, View, Dimensions } from 'react-native';
+import MapView, { Polygon } from 'react-native-maps';
+import { cartiere } from '../constants/cartiere';
+import mapStyle from '../constants/mapStyle';
+
+const MapScreen = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 44.4268,
+          longitude: 26.1025,
+          latitudeDelta: 0.3,
+          longitudeDelta: 0.3,
+        }}
+        customMapStyle={mapStyle}
+      >
+        {cartiere.map((item) => (
+          <Polygon key={item.name} coordinates={item.coordinates} fillColor={'#3aeb3444'} strokeColor={'#f59ad8'} />
+        ))}
+      </MapView>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  map: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
+
+export default MapScreen;
