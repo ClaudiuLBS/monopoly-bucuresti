4a4bb771ef6d6f4897c4f8f2449441de89a8f70e
Claudiu <clau_123@yahoo.com>
2022-08-02 19:59:57 +0300

    create game service thate handles mechanics endpoints

---
+++ services/game.service.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+const GameService = {
+  async findLocation(latitude, longitude, code) {
+    const result = await axios
+      .post(`${config.url}/find-location/`, { latitude, longitude, code })
+      .catch((e) => ({
+        property: null,
+        price: null,
+      }));
+
+    return result.data;
+  },
+
+  async buyProperty(player, property) {
+    const result = await axios
+      .post(`${config.url}/buy-property/`, { player, property })
+      .catch((e) => ({ property_id: null }));
+
+    return result.data;
+  },
+
+  async bringSoldiers(player, property, count) {
+    const result = await axios
+      .post(`${config.url}/bring-soldiers/`, { player, property, count })
+      .catch((e) => ({ soldiers: null }));
+
+    return result.data;
+  },
+
+  async dropSoldiers(player, property, count) {
+    const result = await axios
+      .post(`${config.url}/drop-soldiers/`, { player, property, count })
+      .catch((e) => ({ soldiers: null }));
+
+    return result.data;
+  },
+
+  async attack(player, property) {
+    const result = await axios
+      .post(`${config.url}/attack-property/`, { player, property })
+      .catch((e) => ({ win: undefined }));
+
+    return result.data;
+  },
+};
+
+// path('find-location/', find_location, name='find-location'),
+// path('buy-property/', buy_property, name='buy-property'),
+// path('attack-property/', attack_property, name='attack-property'),
+// path('bring-soldiers/', bring_soldiers, name='bring-soldiers'),
+// path('drop-soldiers/', drop_soldiers, name='drop-soldiers'),
+export default GameService;
